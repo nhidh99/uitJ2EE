08cd2bc1b8a21078d6bf75120bcf05236e668408
nhidh99 <nhidh99@gmail.com>
2020-04-13 13:59:47 +0700

    Update dependencies, create JAX-RS without JBoss & Resteasy framework

--- examples/ejb-jaxrs-example/src/main/java/org/example/service/impl/UserServiceImpl.java
+++ examples/ejb-jaxrs-example/src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package org.example.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.dao.api.UserDAO;
 import org.example.model.User;
 import org.example.service.api.UserService;
@@ -18,12 +19,19 @@ public class UserServiceImpl implements UserService {
     @EJB(mappedName = "UserDAOImpl")
     private UserDAO userDAO;
 
+    private final ObjectMapper om = new ObjectMapper();
+
     @Override
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response findAllUser() {
-        List<User> users = userDAO.findAll();
-        return Response.ok(users).build();
+    public Response findAllUser()  {
+        try {
+            List<User> users = userDAO.findAll();
+            String usersJSON = om.writeValueAsString(users);
+            return Response.ok(usersJSON).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
